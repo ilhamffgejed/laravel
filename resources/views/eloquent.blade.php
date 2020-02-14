0433336e5947fc5b8598061f196a8271f494a60e
ilhamffgejed <phpajah41@gmail.com>
2020-02-14 11:11:27 +0700

    latihan-crud

--- resources/views/eloquent.blade.php
+++ resources/views/eloquent.blade.php
@@ -7,19 +7,19 @@
     <title>Document</title>
 </head>
 <body class="container">
-		<div class="col-sm-8 col-sm-offset-2">
+		<!-- <div class="col-sm-8 col-sm-offset-2">
 			@foreach ($data as $temp)
-				<h3> <small>{{$temp->nama}} [{{$temp->nim}}]</small></h3>
+				<h3> <small>{{$temp->nama}} [{{$temp->nim}}]</small></h3> -->
 				<h5>Hobi : 
 					@foreach($temp->hobi as $tampung) 
 						<small>{{$tampung->hobi}} </small> 
 					@endforeach
 				</h5>
-				<h4>
+				<!-- <h4>
 					<li>Nama Wali : {{$temp->wali->nama}}<strong></strong></li>
 					<li>Dosen Pembimbing : <strong>{{$temp->dosen->nama}}</strong></li>
 				</h4>
 				<hr/>
 			@endforeach
-		</div>
+		</div> -->
 	</body></html>
